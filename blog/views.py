0eb21d03ac91738b0c4db9748289325c42fd5d35
Eddges <shivamshekhar.i7@gmail.com>
2020-10-31 16:03:01 +0530

    Jinja system added and iterations made

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,26 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Shivam Shekhar',
+        'title': 'Blog Post 1',
+        'content': 'This is the first blog post. So excited to be a part',
+        'date_posted': 'October 31, 2020'
+    },
+    {
+        'author': 'Superman',
+        'title': 'Ispaiderman',
+        'content': 'Spiderman is a piece of shit superhero. I am the only OP superhero, and that is a fact',
+        'date_posted': 'October 5, 2020'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
