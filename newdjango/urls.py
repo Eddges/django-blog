9becda832b19dff3fd31a77e44623338faaa24c0
Eddges <shivamshekhar.i7@gmail.com>
2020-11-06 16:08:11 +0530

    Register form added from UserCreationForm, template added

--- newdjango/urls.py
+++ newdjango/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users.views import register as users_register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', users_register),
     path('blog/', include('blog.urls')),
 ]
